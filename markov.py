71b13c7ce7ff7f71c95ea04cab0065d9775fec21
Hackbright Student <info@hackbrightacademy.com>
2015-04-17 12:59:57 -0700

    Final Markov Chain!

--- markov.py
+++ markov.py
@@ -1,13 +1,6 @@
 import sys
 import random
 
-# maya_poem = open("maya-angelou.txt")
-# maya_string = maya_poem.read(len(maya_poem))
-# for word in maya_poem:
-#     word = word.rstrip()
-#     word = word.split(' ')
-# print maya_poem
-
 def make_chains(corpus):
     """Takes input text as string; returns dictionary of markov chains."""
     split_text = corpus.split()
@@ -23,60 +16,47 @@ def make_chains(corpus):
         else:
             chain_dict[key].append(value) 
 
-    # print chain_dict
-    # print split_text
-
     return chain_dict
 
 
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
-    word_list = []
 
     # Step 1: Choose random key from chains
     current_key = random.choice(chains.keys())
-    # print current_key
+    word_list = [current_key[0], current_key[1]]
     # Step 2: Put words of random key into a list
-    word_list.append(current_key)
-   
-    print word_list
     # sub-Step 2: We should see a list that has two words
-    # print word_list
     # sub-sub-Step 2: Two words should be the word from tuple
     # Step 3: With two words in word list, use a while loop, 
             # probably key in dictionary.
-    current_key_value = (chain_dict[value])
-    
-    while current_key in chain_dict:
-        word_list.append(current_key_value)
-        new_key = (current_key[1],current_key_value)
-        current_key = new_key
-     
-        print current_key
-
+    while current_key in chains:  
     # Step 4: While in while loop, choose a value for the key
+        word = random.choice(chains[current_key])
     # while current_key in chains 
     # Step 5: Place value, append to word list
+        word_list.append(word)
     # Step 6: Make a new key that will be the second word of the current key for our next value.
     # current_key = (current_key[1], value) 
-    print word_list
-    return str(word_list)
+        current_key = (current_key[1], word)
 
+    return " ".join(word_list)
 
 # # Change this to read input_text from a file, deciding which file should
 # # be used by examining the `sys.argv` arguments (if neccessary, see the
 # # Python docs for sys.argv)
 
-print sys.argv[1]
 # input_text = "Some text"
-input_file = open(sys.argv[1])
-# input_file = open('green-eggs.txt')
-input_text = input_file.read()
-#print input_text                                                                                                                    
+# input_file = open(sys.argv[1])
+input_file = open('green-eggs.txt')
+input_text = input_file.read() 
+
 # Get a Markov chain
-chain_dict = make_chains(input_text)
+chains = make_chains(input_text)
 
 # # Produce random text
-random_text = make_text(chain_dict)
+random_text = make_text(chains)
+print random_text
 
-#print random_text
+
+
